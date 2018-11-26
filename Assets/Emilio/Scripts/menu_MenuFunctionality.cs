9f18e9ede87d7423f4f294f680cf13f44ab9ff91
EmilioMojica <38330934+EmilioMojica@users.noreply.github.com>
2018-11-26 09:43:43 -0500

    Updates (nothing substantial yet)

--- Assets/Emilio/Scripts/menu_MenuFunctionality.cs
+++ Assets/Emilio/Scripts/menu_MenuFunctionality.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class menu_MenuFunctionality : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    public GameObject MainMenu;
+    [SerializeField]
+    public GameObject PauseMenu;
+    [SerializeField]
+    public GameObject OptionsMenu;
+    [SerializeField]
+    public GameObject Credits;
+    [SerializeField]
+    public GameObject InGameUI;
+    [SerializeField]
+    public GameObject ModeSelect;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
