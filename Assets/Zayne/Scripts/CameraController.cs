cd347f9f46bc28c78581e45796585a728a3e45e4
ztnynedu <44003908+ztnynedu@users.noreply.github.com>
2018-12-11 05:45:08 -0500

    zayne
    added camera controller

---
+++ Assets/Zayne/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraController : NetworkBehaviour
+{
+    public Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam.setactive(true);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
